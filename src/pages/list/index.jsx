046c036e22b365cf3dc773c2420d4d7bb49cb2ed
nosetrix <emanuel.gotstark@gmail.com>
2020-12-09 20:28:17 -0300

    fix: loop em lista

--- src/pages/list/index.jsx
+++ src/pages/list/index.jsx
@@ -1,35 +1,36 @@
-import { useState } from "react";
+import { useState } from "react"
 
-import { useSelector } from "react-redux";
+import { useSelector } from "react-redux"
 
-import axios from "axios";
+import axios from "axios"
 
 const List = () => {
-  const [seek, setSeek] = useState(null);
-  const [searchResult, setSearchResult] = useState(undefined);
-  const [search, setSearch] = useState("");
-  const listOfUsers = useSelector((state) => state.listOfUsers);
+  const [seek, setSeek] = useState(null)
+  const [searchResult, setSearchResult] = useState(undefined)
+  const [search, setSearch] = useState("")
+  const listOfUsers = useSelector((state) => state.listOfUsers)
 
   const handleSearchButton = () => {
     //busca na API com o nome salvo no estado search e altera o estado searchResult;
-    setSeek(true);
-    setSearchResult(listOfUsers.find((user) => search === user.name));
+    setSeek(true)
+    setSearchResult(listOfUsers.find((user) => search === user.name))
     if (searchResult) {
-      axios.get(`https://kenziehub.me/users/${searchResult.id}`);
+      axios.get(`https://kenziehub.me/users/${searchResult.id}`)
       //trata o dado de acordo recebido da API e salva no estado searchResult
-      return setSeek(null);
+      return setSeek(null)
     }
-    setSeek(false);
-    setSearch("");
-  };
+    setSeek(false)
+    setSearch("")
+  }
 
   const handleSearchInput = (ev) => {
     // pegar valor digitado no input
-    setSearch(ev.target.value);
-  };
+    setSearch(ev.target.value)
+  }
 
   return (
     <>
+      list
       {seek === true ? (
         <div>Loading...</div>
       ) : seek === null ? (
@@ -65,7 +66,7 @@ const List = () => {
         <div>No match found</div>
       )}
     </>
-  );
-};
+  )
+}
 
-export default List;
+export default List
