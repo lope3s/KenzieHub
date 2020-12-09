39fdfb560bae994f37c32e6b73920ddd4faf66ed
nosetrix <emanuel.gotstark@gmail.com>
2020-12-08 21:22:02 -0300

    feat: criando rotas

---
+++ src/routes/index.jsx
@@ -0,0 +1,36 @@
+import { Switch, Route } from "react-router-dom"
+import Home from "../pages/home"
+import List from "../pages/list"
+import Profile from "../pages/profile"
+import SignIn from "../pages/signIn"
+import SingnUp from "../pages/signUp"
+
+const Routes = (isAuth) => {
+  if (isAuth) {
+    return (
+      <>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/list" component={List} />
+          <Route exact path="/profile" component={Profile} />
+          <Route exact path="/signIn" component={SignIn} />
+          <Route exact path="/signUp" component={SingnUp} />
+        </Switch>
+      </>
+    )
+  }
+  if (!isAuth) {
+    return (
+      <>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/list" component={List} />
+          <Route exact path="/signIn" component={SignIn} />
+          <Route exact path="/signUp" component={SingnUp} />
+        </Switch>
+      </>
+    )
+  }
+}
+
+export default Routes
