ffc600fefd4514ef8ade21ca58bf687602fcbc7d
João Victor <jb-direito@hotmail.com>
2020-12-09 15:46:17 -0300

    Link de registro

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom"
+
+export const Home = () => {
+    return(
+        <>
+        <div>
+        <Link to="/signUp">Registration Area</Link>
+        </div>
+        </>
+    )
+}
