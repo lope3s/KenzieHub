47f0810bd777d27dce3f66193d845371336765d5
Alexandre Alfa <Profissional@alexandrealfa.com>
2020-12-10 15:08:56 -0400

    feat : inseri a funçao e o botao no componente login.

--- src/components/login/index.jsx
+++ src/components/login/index.jsx
@@ -1,25 +1,54 @@
 import { useForm } from "react-hook-form";
-
+import {useState} from 'react'
 import { TextField } from "@material-ui/core";
 
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 
+import axios from 'axios'
+
+// import {useHistory} from "react-router-dom"
+
 const Login = () => {
+
+   // const history = useHistory()
+   const [error,setError] = useState(false)
+
   const schema = yup.object().shape({
     userName: yup.string().required("Cannot be blank"),
     password: yup.string().required("Cannot be blank"),
   });
 
-  const { register, errors } = useForm({
+  const { register, errors,handleSubmit } = useForm({
     resolver: yupResolver(schema),
   });
+  const sendData = async(body) =>{
+    body = {email:body.userName, password:body.password}
+
+    try{
+      const res = await axios.post(
+        "https://kenziehub.me/sessions",
+        body
+      )
+      const data = await res.data
+      setError(false)
+      window.localStorage.setItem("infoLogged", JSON.stringify(data));
+      // setar componente no state global  ok
+      // history.push(/profile)
+    }
+    catch(err){
+      setError(true)
+      console.log(err)
+    }
+
+  }
 
   return (
     <div>
       <fieldset>
         <legend>Login</legend>
-        <form>
+        {error && <div>Não foi possivel realizar seu Login, tente novamente</div>}
+        <form onSubmit={handleSubmit(sendData)}>
           <TextField
             margin="normal"
             variant="outlined"
