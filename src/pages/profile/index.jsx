73bac16ed9e78d1241be45ae3795b7e059615239
LeandroLino <leao.lino7@gmail.com>
2020-12-10 15:18:12 -0300

    feature

--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -1,5 +1,64 @@
+import { User } from "./User";
+import { Container, ContainerContact } from "./styles";
 const Profile = () => {
-  return <div>Profile</div>
-}
+  return (
+    <div>
+      <Container>
+        {User.map((element, index) => (
+          <div key={index}>
+            <div>
+              <img alt={element.user.name} src={element.user.avatar_url}></img>
+            </div>
+            <div>
+              <div>
+                <span>{element.user.name}</span>
+              </div>
+            </div>
+          </div>
+        ))}
+      </Container>
+      {User.map((element) => (
+        <div>
+          <div>
+            <div>About Me</div>
+            <div>{element.user.bio}</div>
+          </div>
 
-export default Profile
+          <div>
+            <div>Personal Details</div>
+            <div>
+              <div>Email:{element.user.email}</div>
+              <div>Course Module: {element.user.course_module}</div>
+            </div>
+          </div>
+
+          <div>Techs</div>
+          <div>
+            {element.user.techs.map((element, index) => (
+              <div key={index}>
+                {element.title} - {element.status}
+              </div>
+            ))}
+          </div>
+        </div>
+      ))}
+      {User.map((element) => (
+        <div>
+          <div>
+            <div>Works</div>
+            <div>
+              {element.user.works.map((element) => (
+                <div>
+                  <p>{element.title}</p>
+                  <div>{element.description}</div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Profile;
---
+++ src/pages/profile/styles.js
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+  > div div img {
+    width: 150px;
+    height: 150px;
+    display: flex;
+    border-radius: 50%;
+  }
+
+  > div div div {
+    > span {
+      color: white;
+      margin-top: 100px;
+      font-size: 25px;
+      font-family: "Montserrat", sans-serif;
+    }
+    background-color: #081648;
+    background-size: cover;
+
+    position: absolute;
+    top: 10%;
+
+    height: 30%;
+    width: 25%;
+
+    left: 37.5%;
+    right: 37.5%;
+
+    z-index: -1;
+    border-radius: 5px;
+
+    justify-content: center;
+    display: flex;
+  }
+`;
+export const ContainerContact = styled.span`
+  > span img {
+    width: 50px;
+    height: 50px;
+  }
+`;
