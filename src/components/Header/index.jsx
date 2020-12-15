c4c141ad7d4139e677d7056fb688b61384cf4ab2
Alexandre Alfa <Profissional@alexandrealfa.com>
2020-12-15 16:38:11 -0400

    inserir ajustes nos botoes da aplicaçao

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react"
-
+import {useLocation} from 'react-router-dom'
 import { useSelector, useDispatch } from "react-redux"
 import { validation } from "../../store/modules/validationToken/action"
 
@@ -7,6 +7,8 @@ import { HeaderContent, LinksContent } from "./style"
 import ButtonRedirect from "../../components/ButtonRedirect"
 
 const Header = () => {
+  const location = useLocation();
+console.log(location)
   const [isAuth, setAuth] = useState(false)
   const auth = useSelector((state) => state.auth)
   const dispatch = useDispatch()
@@ -20,7 +22,11 @@ const Header = () => {
     <LinksContent>
       <div className="link-home">
         <ButtonRedirect text={"Home"} link={""} />
+        
       </div>
+      {location.pathname === '/profile' && <ButtonRedirect text={"List"} link={"list/page=1"} />}
+      {location.pathname === '/' && <ButtonRedirect text={"Profile"} link={"profile"} />}
+      {location.pathname.slice(0,5) === '/list' && <ButtonRedirect text={"Voltar"} link={"profile"} />}
       <button onClick={logOut}>Log Out</button>
     </LinksContent>
   )
@@ -28,6 +34,7 @@ const Header = () => {
     <LinksContent>
       <div className="link-home">
         <ButtonRedirect text={"Home"} link={""} />
+        {location.pathname === '/profile' && <ButtonRedirect text={"List"} link={"list/page=1"} />}
       </div>
       <ButtonRedirect text={"Sign In"} link={"signIn"} />
       <ButtonRedirect text={"Sign Up"} link={"signUp"} />
