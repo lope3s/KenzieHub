5ad412861d3baac9ab8d46cd6a2fc9146c9ef9af
nosetrix <emanuel.gotstark@gmail.com>
2020-12-16 19:08:33 -0300

    fix: cor list

--- src/pages/list/style.js
+++ src/pages/list/style.js
@@ -1,32 +1,40 @@
-import styled from "styled-components";
+import styled from "styled-components"
 
 export const UsersPageContainer = styled.div`
   width: 100vw;
   height: 75vh;
   overflow-y: auto;
   overflow-x: hidden;
-  @media (min-width: 320px) and (max-width: 768px){
+  @media (min-width: 320px) and (max-width: 768px) {
     height: 70vh;
   }
-`;
+`
 export const PaginationContainer = styled.div`
   margin: 0 5vw 0 5vw;
   display: flex;
   align-items: center;
   justify-content: space-between;
 
-  @media (min-width: 320px) and (max-width: 768px){
+  nav > ul > li > button {
+    color: #fff;
+  }
+
+  .MuiInputBase-root {
+    color: #fff;
+  }
+
+  @media (min-width: 320px) and (max-width: 768px) {
     margin: 2vw;
 
     nav > ul > li > button {
       min-width: 0.6rem;
     }
   }
-`;
+`
 
 export const SearchContainer = styled.div`
   display: flex;
-  justify-content:  center;
+  justify-content: center;
 
   width: 100vw;
 
@@ -35,37 +43,36 @@ export const SearchContainer = styled.div`
     margin-left: 3vw;
     height: 56px;
     align-self: center;
-    background-color: #48A3DA;
+    background-color: #48a3da;
     min-width: 15vw;
     border-radius: 10px;
-    font-family:"Righteous", cursive;
+    font-family: "Righteous", cursive;
 
-   :hover{
-    background-color: #48A3DA ;
-    border: solid 1px #fff;
-   }
+    :hover {
+      background-color: #48a3da;
+      border: solid 1px #fff;
+    }
   }
 
-  .MuiInputBase-root{
+  .MuiInputBase-root {
     background-color: #fff;
     border-radius: 10px;
   }
 
   input {
     width: 55vw;
-    font-family:"Righteous", cursive;
+    font-family: "Righteous", cursive;
   }
 `
 
 export const UsersContainerTitle = styled.div`
   margin: 0 13vw 0 13vw;
-  display:flex;
+  display: flex;
   justify-content: space-around;
   color: #fff;
-  font-family:"Righteous", cursive;
+  font-family: "Righteous", cursive;
 
-  @media (min-width: 320px) and (max-width: 768px){
+  @media (min-width: 320px) and (max-width: 768px) {
     margin: 0px;
   }
-
 `
